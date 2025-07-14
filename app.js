8cb1362b04dd79d5bbd8736256dca096fe98601b
Ekramkhan <ekramk3@gmail.com>
2025-07-14 21:29:39 +0600

    Day 03

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-let mobileSearchIcon = document.querySelector(".mobile-search");
-let mobileSearchscreen = document.querySelector("#search");
-let searchClose = document.querySelector(".search-close")
+$(function(){
+    let mobileSearchIcon = document.querySelector(".mobile-search");
+    let mobileSearchscreen = document.querySelector("#search");
+    let searchClose = document.querySelector(".search-close")
 
-mobileSearchIcon.addEventListener('click', function(){
-    mobileSearchscreen.classList.add("search_active");
-});
+    mobileSearchIcon.addEventListener('click', function(){
+        mobileSearchscreen.classList.add("search_active");
+    });
 
-searchClose.addEventListener('click', function(){
-    mobileSearchscreen.classList.remove("search_active");
+    searchClose.addEventListener('click', function(){
+        mobileSearchscreen.classList.remove("search_active");
+    })
+
+    $(".sliders").slick({
+      dots: true,
+      slidesToShow: 1,
+      arrows: true,
+      nextArrow: `<span class="next"><iconify-icon icon="mingcute:arrow-right-line" width="24" height="24"></iconify-icon></span>`,
+      prevArrow: `<span class="prev"><iconify-icon icon="lets-icons:arrow-left" width="24" height="24"></iconify-icon></span>`,
+      autoplay: true,
+      autoplaySpeed: 2500,
+    });
+
 })
