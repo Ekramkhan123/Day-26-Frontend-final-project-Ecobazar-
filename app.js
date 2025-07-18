ac965bd0e14ef5648432234ed400c74774dbeaea
Ekramkhan <ekramk3@gmail.com>
2025-07-18 22:57:36 +0600

    Day 04

--- app.js
+++ app.js
@@ -21,4 +21,48 @@ $(function(){
       autoplaySpeed: 2500,
     });
 
+    $(".product_parent_sliders").slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      dots:false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots:false,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+          },
+        },
+      ],
+    });
+
+    const tooltipTriggerList = document.querySelectorAll(
+      '[data-bs-toggle="tooltip"]'
+    );
+    const tooltipList = [...tooltipTriggerList].map(
+      (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+    );
+
 })
