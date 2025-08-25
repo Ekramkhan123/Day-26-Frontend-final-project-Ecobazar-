a2c978118330dcc39bad297f77bdcb4a6ef4fc8f
Ekramkhan123 <ekramk3@gmail.com>
2025-08-26 03:44:38 +0600

    page 10 day 1

--- vegetable_app.js
+++ vegetable_app.js
@@ -9,7 +9,39 @@ $(function(){
 
     searchClose.addEventListener('click', function(){
         mobileSearchscreen.classList.remove("search_active");
-    })
+    });
+
+  let categoriesDiv = document.querySelector(".catagories");
+  let categoriesContent = document.querySelector(".catagories_content");
+
+  categoriesDiv.addEventListener("click", function () {
+    categoriesContent.classList.toggle("show");
+    categoriesDiv.classList.toggle("active"); // for rotating icon
+  });
+
+  let progress_btn = document.querySelector(".progress_heading");
+  let progressContent = document.querySelector(".progress_bar");
+
+  progress_btn.addEventListener("click", function () {
+    progressContent.classList.toggle("show");
+    progress_btn.classList.toggle("active"); // for rotating icon
+  });
+
+  let rating_btn = document.querySelector(".rating_heading");
+  let ratingContent = document.querySelector(".rating_content");
+
+  rating_btn.addEventListener("click", function () {
+    ratingContent.classList.toggle("show");
+    rating_btn.classList.toggle("active"); // for rotating icon
+  });
+
+  let tag_btn = document.querySelector(".tags_heading");
+  let tagContent = document.querySelector(".tag_option");
+
+  tag_btn.addEventListener("click", function () {
+    tagContent.classList.toggle("show");
+    tag_btn.classList.toggle("active"); // for rotating icon
+  });
 
     const tooltipTriggerList = document.querySelectorAll(
       '[data-bs-toggle="tooltip"]'
@@ -21,7 +53,7 @@ $(function(){
 
     $('.pagination').slick({
     centerMode: false,
-    centerPadding: '20px',
+    centerPadding: '0px',
     slidesToShow: 7,
     arrows: true ,
     dot: true,
